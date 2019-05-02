72c684aa842192835d6b4ef22f008de20eafc898
Ziad Mostafa <mohammedziad.badr@gmail.com>
2019-05-02 11:27:54 +0200

    Fix cmake project and add first lesson code

---
+++ foundations/lesson-2/src/main.cpp
@@ -0,0 +1,70 @@
+/*******************************************************************************
+/  @author Ziad Mostafa                                                        /
+/  @Date 20.04.2019                                                            /
+/  @filename main.cpp                                                          /
+/                                                                              /
+/                                                                              /
+*******************************************************************************/
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using std::cout;
+using std::ifstream;
+using std::istringstream;
+using std::string;
+using std::vector;
+
+enum class State {kEmpty, kObstacle};
+
+vector<State> ParseLine(string line) {
+    istringstream sline(line);
+    int n;
+    char c;
+    vector<State> row;
+    while (sline >> n >> c && c == ',') {
+      if (n == 0) {
+        row.push_back(State::kEmpty);
+      } else {
+        row.push_back(State::kObstacle);
+      }
+    }
+    return row;
+}
+
+vector<vector<State>> ReadBoardFile(string path) {
+  ifstream myfile (path);
+  vector<vector<State>> board{};
+  if (myfile) {
+    string line;
+    while (getline(myfile, line)) {
+      vector<State> row = ParseLine(line);
+      board.push_back(row);
+    }
+  }
+  return board;
+}
+
+string CellString(State cell) {
+  switch(cell) {
+    case State::kObstacle: return "⛰️   ";
+    default: return "0   "; 
+  }
+}
+
+void PrintBoard(const vector<vector<State>> board) {
+  // Use unsigned int to fix error of comaprison between int and unsigned int 
+  for (unsigned i = 0; i < board.size(); i++) {
+    for (unsigned j = 0; j < board[i].size(); j++) {
+      cout << CellString(board[i][j]);
+    }
+    cout << "\n";
+  }
+}
+
+int main() {
+  auto board = ReadBoardFile("1.board");
+  PrintBoard(board);
+}
